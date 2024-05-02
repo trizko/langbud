0878270602692933d5030872ef0bda1c5f2e4ace
Tony Rizko <tony@rizkocircle.com>
2024-05-01 21:55:07 -0700

    Reformat all files with ruff linter

--- src/models/user.py
+++ src/models/user.py
@@ -10,7 +10,13 @@ class User(BaseModel):
     learning_language: str
     messages: [dict]
 
-    def __init__(self, username: str, spoken_language: str, learning_language: str, messages: [dict]):
+    def __init__(
+        self,
+        username: str,
+        spoken_language: str,
+        learning_language: str,
+        messages: [dict],
+    ):
         self.username = username
         self.spoken_language = spoken_language
         self.learning_language = learning_language
