ba4e5d6433acc78e74c670630ff30bb40336204b
Tony Rizko <tony@rizkocircle.com>
2024-05-01 21:52:06 -0700

    Add a user model

---
+++ src/models/user.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    username: str
+    spoken_language: str
+    learning_language: str
+    messages: [dict]
+
+    def __init__(self, username: str, spoken_language: str, learning_language: str, messages: [dict]):
+        self.username = username
+        self.spoken_language = spoken_language
+        self.learning_language = learning_language
+        self.messages = messages
