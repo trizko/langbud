f549efba0c5a2dc733849e32cd9776f26e5cb150
Tony Rizko <tony@rizkocircle.com>
2024-05-08 22:35:46 -0700

    Add variables.tf file for loading api key and ssh fingerprint

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "do_token" {
+  type        = string
+  default     = ""
+  description = "DigitalOcean API Token"
+}
+
+variable "ssh_fingerprint" {
+  type        = string
+  default     = ""
+  description = "SSH Key Fingerprint"
+}
