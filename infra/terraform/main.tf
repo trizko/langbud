69333783e0fa0f9649f4b1493d653fbd928c4eaa
Tony Rizko <tony@rizkocircle.com>
2024-05-08 22:38:21 -0700

    Add main.tf file for spinning up web server

---
+++ infra/terraform/main.tf
@@ -0,0 +1,16 @@
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_droplet" "web_server" {
+  name   = "web-server"
+  image  = "debian-10-x64"
+  region = "sfo3"
+  size   = "s-1vcpu-1gb"
+
+  user_data = file("${path.module}/user-data.sh")
+
+  ssh_keys = [
+    var.ssh_fingerprint
+  ]
+}
