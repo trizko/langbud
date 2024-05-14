d2f251042c16592275a2802a4d03a1600ba2607a
Tony Rizko <tony@rizkocircle.com>
2024-05-13 22:02:18 -0700

    Add remote state backend for terraform deployments

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 2.0"
     }
   }
+  backend "s3" {
+    endpoint                    = "sfo3.digitaloceanspaces.com"
+    key                         = "terraform.tfstate"
+    bucket                      = "langbud-terraform-state"
+    region                      = "us-east-1"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+  }
 }
 
 provider "digitalocean" {
