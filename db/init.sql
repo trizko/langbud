c2ca3dc81084ac0937ebcb02242914de627568d5
Tony Rizko <tony@rizkocircle.com>
2024-05-14 22:43:38 -0700

    Add messages table with fk relationship to users

--- db/init.sql
+++ db/init.sql
@@ -3,7 +3,6 @@ CREATE TABLE users (
     username VARCHAR(255) NOT NULL,
     spoken_languages JSON,
     learning_languages JSON,
-    messages JSON,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -22,3 +21,12 @@ CREATE TRIGGER update_modified_at_before_update
 BEFORE UPDATE ON ratings
 FOR EACH ROW
 EXECUTE FUNCTION update_modified_at();
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    message_text TEXT NOT NULL,
+    is_from_user BOOLEAN NOT NULL,
+    sent_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
