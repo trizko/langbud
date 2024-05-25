2fd94c017c9590a5957afc77a2392b6a5b23825d
Tony Rizko <tony@rizkocircle.com>
2024-05-25 14:07:03 -0700

    Update downgrade migration to include index

--- migrations/001_initial_down.sql
+++ migrations/001_initial_down.sql
@@ -1,4 +1,5 @@
+DROP TABLE IF EXISTS messages;
 DROP TRIGGER IF EXISTS update_modified_at_before_update ON users;
 DROP FUNCTION IF EXISTS update_modified_at;
-DROP TABLE IF EXISTS messages;
+DROP INDEX IF EXISTS idx_users_username;
 DROP TABLE IF EXISTS users;
