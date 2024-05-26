dc7f3ecb98aac5fc97fa1eb3805899b97850b20f
Tony Rizko <tony@rizkocircle.com>
2024-05-26 15:45:29 -0700

    Implement get_user_by_username and create_user methods

--- src/models/user.py
+++ src/models/user.py
@@ -11,20 +11,27 @@ class User(BaseModel):
     messages: List[dict]
 
 
-async def create_user(self):
+async def create_user(db_conn: asyncpg.Connection, username: str, spoken_language: str, learning_language: str):
+    new_user = await db_conn.fetchrow(
+        "INSERT INTO users (username, spoken_language, learning_language) VALUES ($1, $2, $3)",
+        username,
+        spoken_language,
+        learning_language
+    )
+    return new_user
+
+async def get_user_by_username(db_conn: asyncpg.Connection, username: str):
+    user = await db_conn.fetchrow("SELECT * FROM users WHERE username = $1", username)
+    return user
+
+async def get_user(db_conn: asyncpg.Connection, user_id: int):
     pass
 
-async def get_user(self, user_id: int):
+async def update_user(db_conn: asyncpg.Connection, user_id: int):
     pass
 
-async def get_user_by_username(self, username: str):
+async def delete_user(db_conn: asyncpg.Connection, user_id: int):
     pass
 
-async def update_user(self, user_id: int):
-    pass
-
-async def delete_user(self, user_id: int):
-    pass
-
-async def get_user_messages(self, user_id: int):
+async def get_user_messages(db_conn: asyncpg.Connection, user_id: int):
     pass
