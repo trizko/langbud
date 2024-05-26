ffff55845f023a4b951f667a6bd8e6347acd2abd
Tony Rizko <tony@rizkocircle.com>
2024-05-26 15:42:48 -0700

    Create package for managing db connection pools

---
+++ src/db/database.py
@@ -0,0 +1,19 @@
+import os
+import asyncpg
+
+
+class Database:
+    def __init__(self, dsn: str = None):
+        self._pool = None
+        self.dsn = dsn
+
+    async def connect(self):
+        self._pool = await asyncpg.create_pool(dsn=self.dsn)
+
+    async def disconnect(self):
+        await self._pool.close()
+
+    async def get_pool(self):
+        if self._pool is None:
+            await self.connect()
+        return self._pool
