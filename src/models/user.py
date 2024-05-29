3d01dfc16ceacb2d4dd7fa43c7692aa5559d11dd
Tony Rizko <tony@rizkocircle.com>
2024-05-28 22:12:15 -0700

    Implement update_user method

--- src/models/user.py
+++ src/models/user.py
@@ -42,8 +42,29 @@ async def get_user(db_conn: asyncpg.Connection, user_id: int):
     if not user:
         return None
 
-async def update_user(db_conn: asyncpg.Connection, user_id: int):
-    pass
+    return User(
+        user_id=user.get("id"),
+        username=user.get("username"),
+        spoken_language=user.get("spoken_language"),
+        learning_language=user.get("learning_language")
+    )
+
+async def update_user(db_conn: asyncpg.Connection, user_id: int, spoken_language: str, learning_language: str):
+    user = await db_conn.fetchrow(
+        "UPDATE users SET spoken_language = $1, learning_language = $2 WHERE id = $3 RETURNING *",
+        spoken_language,
+        learning_language,
+        user_id
+    )
+    if not user:
+        return None
+
+    return User(
+        user_id=user.get("id"),
+        username=user.get("username"),
+        spoken_language=user.get("spoken_language"),
+        learning_language=user.get("learning_language")
+    )
 
 async def delete_user(db_conn: asyncpg.Connection, user_id: int):
     pass
