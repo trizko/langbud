25e5028d9d2e223b9de2b73a4fea21fb775e56a4
Tony Rizko <tony@rizkocircle.com>
2024-06-05 23:47:24 -0700

    Fix down sql to remove conversations table and indices

--- migrations/001_initial_down.sql
+++ migrations/001_initial_down.sql
@@ -4,6 +4,9 @@ DROP TABLE IF EXISTS explanations;
 
 DROP TABLE IF EXISTS messages;
 
+DROP INDEX IF EXISTS idx_conversations_user_id;
+DROP TABLE IF EXISTS conversations;
+
 DROP TRIGGER IF EXISTS update_modified_at_before_update ON users;
 DROP FUNCTION IF EXISTS update_modified_at;
 DROP INDEX IF EXISTS idx_users_discord_username;
