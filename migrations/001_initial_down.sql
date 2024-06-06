8c58b2a74612449981fed3ac62c0438b57a3ee64
Tony Rizko <tony@rizkocircle.com>
2024-06-05 23:23:14 -0700

    Merge all sql migrations files

--- migrations/001_initial_down.sql
+++ migrations/001_initial_down.sql
@@ -1,4 +1,9 @@
+DROP INDEX IF EXISTS idx_explanations_message_id;
+DROP INDEX IF EXISTS idx_explanations_user_id;
+DROP TABLE IF EXISTS explanations;
+
 DROP TABLE IF EXISTS messages;
+
 DROP TRIGGER IF EXISTS update_modified_at_before_update ON users;
 DROP FUNCTION IF EXISTS update_modified_at;
 DROP INDEX IF EXISTS idx_users_username;
--- migrations/002_add_explain_table_down.sql
+++
@@ -1,3 +0,0 @@
-DROP INDEX IF EXISTS idx_explanations_message_id;
-DROP INDEX IF EXISTS idx_explanations_user_id;
-DROP TABLE IF EXISTS explanations;
