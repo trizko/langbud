151903121a01e85b8561b2432c678591ae3f258a
Tony Rizko <tony@rizkocircle.com>
2024-06-12 22:06:39 -0700

    Add test for update_user method

--- tests/user_test.py
+++ tests/user_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from models.user import User, create_user, get_user, get_user_by_discord_username
+from models.user import User, create_user, get_user, get_user_by_discord_username, update_user
 
 @pytest.mark.asyncio
 async def test_create_user(db_pool):
@@ -31,3 +31,17 @@ async def test_get_user_by_discord_username(db_pool):
         assert user.spoken_language == new_user.spoken_language
         assert user.active_conversation_id == new_user.active_conversation_id
         assert user.user_id == new_user.user_id
+
+@pytest.mark.asyncio
+async def test_update_user(db_pool):
+    async with db_pool.acquire() as connection:
+        new_user = await create_user(connection, "test_update_user", "en")
+        assert new_user.active_conversation_id is None
+        
+        await update_user(connection, new_user.user_id, 420)
+        user = await get_user(connection, new_user.user_id)
+
+        assert user.user_id == new_user.user_id
+        assert user.discord_username == "test_update_user"
+        assert user.spoken_language == "en"
+        assert user.active_conversation_id == 420
