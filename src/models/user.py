d27ba0834e4a40393d2185f9f8442c3095b39d12
Tony Rizko <tony@rizkocircle.com>
2024-06-12 22:22:58 -0700

    Add static method to User class for parsing query results

--- src/models/user.py
+++ src/models/user.py
@@ -12,6 +12,14 @@ class User(BaseModel):
     spoken_language: str
     active_conversation_id: Optional[int]
 
+    def from_query(query_result):
+        return User(
+            user_id=query_result.get("id"),
+            discord_username=query_result.get("discord_username"),
+            spoken_language=query_result.get("spoken_language"),
+            active_conversation_id=query_result.get("active_conversation_id"),
+        )
+
 
 class Message(BaseModel):
     message_id: int
@@ -39,12 +47,7 @@ async def create_user(
         spoken_language,
     )
 
-    return User(
-        user_id=new_user.get("id"),
-        discord_username=new_user.get("discord_username"),
-        spoken_language=new_user.get("spoken_language"),
-        active_conversation_id=new_user.get("active_conversation_id"),
-    )
+    return User.from_query(new_user)
 
 
 async def get_user_by_discord_username(db_conn: asyncpg.Connection, discord_username: str) -> User:
@@ -52,13 +55,7 @@ async def get_user_by_discord_username(db_conn: asyncpg.Connection, discord_user
     if not user:
         return None
 
-    return User(
-        user_id=user.get("id"),
-        discord_username=user.get("discord_username"),
-        spoken_language=user.get("spoken_language"),
-        learning_language=user.get("learning_language"),
-        active_conversation_id=user.get("active_conversation_id"),
-    )
+    return User.from_query(user)
 
 
 async def get_user(db_conn: asyncpg.Connection, user_id: int) -> User:
@@ -66,13 +63,7 @@ async def get_user(db_conn: asyncpg.Connection, user_id: int) -> User:
     if not user:
         return None
 
-    return User(
-        user_id=user.get("id"),
-        discord_username=user.get("discord_username"),
-        spoken_language=user.get("spoken_language"),
-        learning_language=user.get("learning_language"),
-        active_conversation_id=user.get("active_conversation_id"),
-    )
+    return User.from_query(user)
 
 
 async def update_user(
@@ -88,12 +79,19 @@ async def update_user(
     if not user:
         return None
 
-    return User(
-        user_id=user.get("id"),
-        discord_username=user.get("discord_username"),
-        spoken_language=user.get("spoken_language"),
-        active_conversation_id=user.get("active_conversation_id"),
+    return User.from_query(user)
+
+
+async def update_user_language(db_conn: asyncpg.Connection, user: User, spoken_language: str) -> User:
+    user = await db_conn.fetchrow(
+        "UPDATE users SET spoken_language = $1 WHERE id = $2 RETURNING *",
+        spoken_language,
+        user.user_id,
     )
+    if not user:
+        return None
+
+    return User.from_query(user)
 
 
 async def delete_user(db_conn: asyncpg.Connection, user_id: int):
@@ -167,23 +165,6 @@ async def get_last_message(db_conn: asyncpg.Connection, user: User) -> Message:
         is_from_user=message.get("is_from_user"),
         message_text=message.get("message_text"),
         message_language=message.get("message_language"),
-    )
-
-async def update_user_language(db_conn: asyncpg.Connection, user: User, learning_language: str) -> User:
-    user = await db_conn.fetchrow(
-        "UPDATE users SET learning_language = $1 WHERE id = $2 RETURNING *",
-        learning_language,
-        user.user_id,
-    )
-    if not user:
-        return None
-
-    return User(
-        user_id=user.get("id"),
-        discord_username=user.get("discord_username"),
-        spoken_language=user.get("spoken_language"),
-        learning_language=user.get("learning_language"),
-        active_conversation_id=user.get("active_conversation_id"),
     )
 
 async def create_explanation(db_conn: asyncpg.Connection, message: Message, explanation_text: str) -> Explanation:
