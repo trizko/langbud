1921314aef2e8bace9c3251aa3b06a4dc0b8a43b
Tony Rizko <tony@rizkocircle.com>
2024-06-14 21:19:06 -0700

    Add from_query constructor to message model

--- src/models/message.py
+++ src/models/message.py
@@ -7,3 +7,12 @@ class Message(BaseModel):
     conversation_id: int
     is_from_user: bool
     message_text: str
+
+    def from_query(query_result):
+        return Message(
+            message_id=query_result.get("id"),
+            user_id=query_result.get("user_id"),
+            conversation_id=query_result.get("conversation_id"),
+            is_from_user=query_result.get("is_from_user"),
+            message_text=query_result.get("message_text"),
+        )
