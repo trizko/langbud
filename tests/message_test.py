8dd2710179f0530e9edaba4b72c1578a79eea8ef
Tony Rizko <tony@rizkocircle.com>
2024-06-14 22:44:22 -0700

    Fix testing api for message model

--- tests/message_test.py
+++ tests/message_test.py
@@ -1,12 +1,17 @@
 import pytest
 
-from models.message import Message, create_message, get_messages_by_conversation_id, get_messages_by_user_id, get_message, delete_message
+from models.user import User, create_user
+from models.message import Message, create_message, get_last_message_by_user, get_messages_by_conversation_id, get_message, delete_message
 
 
 @pytest.mark.asyncio
 async def test_create_message(db_pool):
     async with db_pool.acquire() as connection:
-        message = await create_message(connection, 1, 1, True, "test_create_message")
+        new_user = await create_user(connection, "test_get_last_message_by_user", "en")
+        conversation = await create_conversation(connection, new_user, "es")
+        user = await get_user(connection, new_user.user_id)
+
+        message = await create_message(connection, user, True, "test_create_message")
         assert message.message_id is not None
         assert message.user_id == 1
         assert message.conversation_id == 1
@@ -15,34 +20,50 @@ async def test_create_message(db_pool):
 
 
 @pytest.mark.asyncio
+async def test_get_last_message_by_user(db_pool):
+    async with db_pool.acquire() as connection:
+        new_user = await create_user(connection, "test_get_last_message_by_user", "en")
+        conversation = await create_conversation(connection, new_user, "es")
+        user = await get_user(connection, new_user.user_id)
+
+        await create_message(connection, user, True, "test_get_last_message_by_user_1")
+        await create_message(connection, user, False, "test_get_last_message_by_user_2")
+        await create_message(connection, user, True, "test_get_last_message_by_user_3")
+        await create_message(connection, user, False, "test_get_last_message_by_user_4")
+
+        message = await get_last_message_by_user(connection, user)
+        assert message.message_id is not None
+        assert message.user_id == user.user_id
+        assert message.conversation_id == conversation.conversation_id
+        assert message.is_from_user == False
+        assert message.message_text == "test_get_last_message_by_user_4"
+
+
+@pytest.mark.asyncio
 async def test_get_messages_by_conversation_id(db_pool):
     async with db_pool.acquire() as connection:
-        await create_message(connection, 1, 1, True, "test_get_messages_by_conversation_id_1")
-        await create_message(connection, 1, 1, True, "test_get_messages_by_conversation_id_2")
+        new_user = await create_user(connection, "test_get_last_message_by_user", "en")
+        conversation = await create_conversation(connection, new_user, "es")
+        user = await get_user(connection, new_user.user_id)
 
-        messages = await get_messages_by_conversation_id(connection, 1)
+        await create_message(connection, user, True, "test_get_messages_by_conversation_id_1")
+        await create_message(connection, user, True, "test_get_messages_by_conversation_id_2")
+
+        messages = await get_messages_by_conversation_id(connection, user.active_conversation_id)
         assert len(messages) == 2
         for message in messages:
             assert message.conversation_id == 1
 
 
 @pytest.mark.asyncio
-async def test_get_messages_by_user_id(db_pool):
-    async with db_pool.acquire() as connection:
-        await create_message(connection, 1, 1, True, "test_get_messages_by_user_id_1")
-        await create_message(connection, 1, 2, True, "test_get_messages_by_user_id_2")
-
-        messages = await get_messages_by_user_id(connection, 1)
-        assert len(messages) == 2
-        for message in messages:
-            assert message.user_id == 1
-
-
-@pytest.mark.asyncio
 async def test_get_message(db_pool):
     async with db_pool.acquire() as connection:
-        message = await create_message(connection, 1, 1, True, "test_get_message")
-        message = await get_message(connection, message.message_id)
+        new_user = await create_user(connection, "test_get_last_message_by_user", "en")
+        conversation = await create_conversation(connection, new_user, "es")
+        user = await get_user(connection, new_user.user_id)
+        new_message = await create_message(connection, user, True, "test_get_message")
+
+        message = await get_message(connection, new_message.message_id)
         assert message.message_id is not None
         assert message.user_id == 1
         assert message.conversation_id == 1
@@ -53,7 +74,11 @@ async def test_get_message(db_pool):
 @pytest.mark.asyncio
 async def test_delete_message(db_pool):
     async with db_pool.acquire() as connection:
-        message = await create_message(connection, 1, 1, True, "test_delete_message")
-        await delete_message(connection, message.message_id)
-        message = await get_message(connection, message.message_id)
+        new_user = await create_user(connection, "test_get_last_message_by_user", "en")
+        conversation = await create_conversation(connection, new_user, "es")
+        user = await get_user(connection, new_user.user_id)
+        new_message = await create_message(connection, user, True, "test_get_message")
+
+        await delete_message(connection, new_message.message_id)
+        message = await get_message(connection, new_message.message_id)
         assert message is None
