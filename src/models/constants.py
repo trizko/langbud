f9bca9e63e0a8f2e95347de98a42b8cc9ae4f7dd
Tony Rizko <tony@rizkocircle.com>
2024-06-21 22:19:50 -0700

    Add support for Swiss German dialect

--- src/models/constants.py
+++ src/models/constants.py
@@ -4,6 +4,7 @@ LANGUAGE_MAPPING = {
     "es-MX": "Mexican Spanish",
     "fr": "French",
     "de": "German",
+    "de-ch": "Swiss German",
     "it": "Italian",
     "tr": "Turkish",
     "pt-BR": "Brazilian Portuguese",
