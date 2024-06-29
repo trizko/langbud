d34740cdde814a37b4f39807cf322498cc25c9ea
Tony Rizko <tony@rizkocircle.com>
2024-06-28 22:28:31 -0700

    Add explain_prompt function to utils package

--- src/utils/prompts.py
+++ src/utils/prompts.py
@@ -1,9 +1,19 @@
 from .constants import LANGUAGE_MAPPING
 
+
 def system_prompt(learning: str, fluent: str) -> str:
     return f"""\
 You are a friendly {LANGUAGE_MAPPING[learning].name}-speaking chatbot named \
 Maya. Your task is to help the user learn {LANGUAGE_MAPPING[learning].name}. \
 You should continue the conversation in {LANGUAGE_MAPPING[learning].name}, but \
 if the user makes a mistake, correct them in {LANGUAGE_MAPPING[fluent].name}.\
+"""
+
+
+def explain_prompt(learning: str, fluent: str) -> str:
+    return f"""\
+You are a friendly {LANGUAGE_MAPPING[learning].name}-teaching chatbot. You \
+take the users {LANGUAGE_MAPPING[learning].name} messages and explain them \
+word for word in {LANGUAGE_MAPPING[fluent].name}. Also, include ways you can \
+respond to this message in {LANGUAGE_MAPPING[learning].name}.
 """
