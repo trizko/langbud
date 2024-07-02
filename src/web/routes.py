4adb3fa5783e576b7253fad1eda13c38c70979cf
Tony Rizko <tony@rizkocircle.com>
2024-07-02 00:51:38 -0700

    Add routes for logging in and redirecting with discord oauth2

--- src/web/routes.py
+++ src/web/routes.py
@@ -1,7 +1,19 @@
+import os
+import httpx
+
 from pydantic import BaseModel
-from fastapi import APIRouter
-from fastapi.responses import FileResponse
+
+from fastapi import APIRouter, Request, HTTPException
+from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+DISCORD_CLIENT_ID = os.getenv("DISCORD_CLIENT_ID")
+DISCORD_CLIENT_SECRET = os.getenv("DISCORD_CLIENT_SECRET")
+DISCORD_REDIRECT_URI = os.getenv("DISCORD_REDIRECT_URI")
 
 
 class UserMessage(BaseModel):
@@ -28,6 +40,42 @@ async def index():
 @router.get('/login/')
 async def login():
     return FileResponse("src/web/frontend/login.html")
+
+
+@router.get("/login-with-discord")
+async def login():
+    return RedirectResponse(url=f"https://discord.com/api/oauth2/authorize?client_id={DISCORD_CLIENT_ID}&redirect_uri={DISCORD_REDIRECT_URI}&response_type=code&scope=identify+email")
+
+
+@router.get("/callback")
+async def callback(request: Request):
+    code = request.query_params.get("code")
+    if not code:
+        raise HTTPException(status_code=400, detail="Code not found in request")
+
+    data = {
+        "client_id": DISCORD_CLIENT_ID,
+        "client_secret": DISCORD_CLIENT_SECRET,
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": DISCORD_REDIRECT_URI
+    }
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post("https://discord.com/api/oauth2/token", data=data, headers=headers)
+        response_data = response.json()
+        access_token = response_data.get("access_token")
+
+        if not access_token:
+            raise HTTPException(status_code=400, detail="Failed to retrieve access token")
+
+        user_response = await client.get("https://discord.com/api/users/@me", headers={"Authorization": f"Bearer {access_token}"})
+        user_data = user_response.json()
+
+    return user_data
 
 
 def setup_routes(app):
