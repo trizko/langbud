c7d1fb89698136b3867a1363144b1b902e539ac4
Tony Rizko <tony@rizkocircle.com>
2024-07-04 13:02:15 -0700

    Add method to connect render to callbacks

--- src/web/frontend/js/app.js
+++ src/web/frontend/js/app.js
@@ -1,18 +1,25 @@
 class App extends HTMLElement {
     constructor() {
-      super();
-      const shadow = this.attachShadow({ mode: 'open' });
-      shadow.innerHTML = `
-        <style>
-          /* Styles for the app */
-        </style>
-        <header-component></header-component>
-        <main>
-          <sidebar></sidebar>
-          <chat></chat>
-        </main>
-      `;
+        super();
+        this.attachShadow({ mode: 'open' });
     }
-  }
 
-  customElements.define('app', App);
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = `
+            <style>
+            /* Styles for the app */
+            </style>
+            <header-component></header-component>
+            <main>
+                <sidebar></sidebar>
+                <chat></chat>
+            </main>
+        `;
+    }
+}
+
+customElements.define('app', App);
