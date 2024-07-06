905f001e509b2f953a2a75028b71699b17cae736
Tony Rizko <tony@rizkocircle.com>
2024-07-05 22:49:07 -0700

    Add header component

---
+++ src/web/frontend/js/header-component.js
@@ -0,0 +1,32 @@
+class Header extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        this.render();
+        this.addEventListeners();
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = `
+            <style>
+            /* Styles for the Header */
+            </style>
+            <header>
+                <h1>Langbud</h1>
+                <nav>
+                    <a href="/profile">Profile</a>
+                    <a href="/settings">Settings</a>
+                </nav>
+            </header>
+        `;
+    }
+
+    addEventListeners() {
+        // Add event listeners here
+    }
+}
+
+customElements.define('header-component', Header);
