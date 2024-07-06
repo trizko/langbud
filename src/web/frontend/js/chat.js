8fb79d5f0b2c71154a4b4e4976989bbc467e4e4d
Tony Rizko <tony@rizkocircle.com>
2024-07-05 22:51:10 -0700

    Add template for chat component

---
+++ src/web/frontend/js/chat.js
@@ -0,0 +1,38 @@
+class Chat extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        this.render();
+        this.addEventListeners();
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = `
+            <style>
+            /* Styles for the chat component */
+            </style>
+            <div>
+                <h2>Chat</h2>
+                <div id="chat"></div>
+                <form>
+                    <input type="text" id="message" placeholder="Message" required>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+        `;
+    }
+
+    addEventListeners() {
+        const form = this.shadowRoot.querySelector('form');
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const message = this.shadowRoot.getElementById('message').value;
+            console.log('Sending message:', message);
+        });
+    }
+}
+
+customElements.define('chat', Chat);
