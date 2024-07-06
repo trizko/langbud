cead2d0c8ba4598f67113cab3679404f57fef7e2
Tony Rizko <tony@rizkocircle.com>
2024-07-05 22:52:53 -0700

    Add template for sidebar component

---
+++ src/web/frontend/js/sidebar.js
@@ -0,0 +1,33 @@
+class Sidebar extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        this.render();
+        this.addEventListeners();
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = `
+            <style>
+            /* Styles for the sidebar component */
+            </style>
+            <div>
+                <h2>Conversations</h2>
+                <ul>
+                    <li>Conversation 1</li>
+                    <li>Conversation 2</li>
+                    <li>Conversation 3</li>
+                </ul>
+            </div>
+        `;
+    }
+
+    addEventListeners() {
+        // Add event listeners here
+    }
+}
+
+customElements.define('sidebar', Sidebar);
