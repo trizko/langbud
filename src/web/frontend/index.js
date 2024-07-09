d4d97304afa907e3953c6357f5559dc8e703d009
Tony Rizko <tony@rizkocircle.com>
2024-07-08 22:37:47 -0700

    Add index.js to bundle all imports

---
+++ src/web/frontend/index.js
@@ -0,0 +1,11 @@
+import { 
+    App,
+    HeaderComponent,
+    Sidebar,
+    Chat
+} from './js/app.js';
+
+customElements.define('app', App);
+customElements.define('header-component', HeaderComponent);
+customElements.define('sidebar', Sidebar);
+customElements.define('chat', Chat);
