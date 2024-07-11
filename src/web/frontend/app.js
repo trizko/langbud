0dab0146a6ff768f2317c221702c93e62de8f78b
Tony Rizko <tony@rizkocircle.com>
2024-07-10 22:03:21 -0700

    Remove HeaderComponent from app

--- src/web/frontend/app.js
+++ src/web/frontend/app.js
@@ -1,9 +1,7 @@
 import { AppComponent } from './js/app-component.js';
-import { HeaderComponent } from './js/header-component.js';
 import { SidebarComponent } from './js/sidebar-component.js';
 import { ChatComponent } from './js/chat-component.js';
 
 customElements.define('app-component', AppComponent);
-customElements.define('header-component', HeaderComponent);
 customElements.define('sidebar-component', SidebarComponent);
 customElements.define('chat-component', ChatComponent);
