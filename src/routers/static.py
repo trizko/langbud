da5b6a93eb63c870666e2fbfdf942d267a09c9e9
Tony Rizko <tony@rizkocircle.com>
2024-07-18 22:31:47 -0700

    Remove discord env variables loading in static router

--- src/routers/static.py
+++ src/routers/static.py
@@ -4,14 +4,6 @@ from fastapi import APIRouter
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
-from dotenv import load_dotenv
-
-
-load_dotenv()
-DISCORD_CLIENT_ID = os.getenv("DISCORD_CLIENT_ID")
-DISCORD_CLIENT_SECRET = os.getenv("DISCORD_CLIENT_SECRET")
-DISCORED_OAUTH2_REDIRECT_URI = os.getenv("DISCORED_OAUTH2_REDIRECT_URI")
-
 
 router = APIRouter()
 router.mount("/static", StaticFiles(directory="src/frontend"), name="static")
