4cffb823a4659b0ac5e941e7010360460a67240e
Tony Rizko <tony@rizkocircle.com>
2024-07-20 13:43:33 -0700

    Add example route that uses db dependency

--- src/routers/api.py
+++ src/routers/api.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.responses import RedirectResponse
 
+from dependencies import get_db_pool
 from models.conversation import create_conversation, get_conversations_by_user_id
 
 
@@ -13,8 +14,10 @@ async def conversation():
 
 
 @router.get("/conversations")
-async def conversations():
-    return get_conversations_by_user_id()
+async def conversations(pool = Depends(get_db_pool)):
+    async with pool.acquire() as connection:
+        conversations = await get_conversations_by_user_id(connection, 1)
+        return conversations
 
 
 @router.get("/logout")
