bdca8e33373c1dd5d036e1e8e65ad5a236b75e06
Tony Rizko <tony@rizkocircle.com>
2024-07-24 19:18:43 -0700

    Add styling to list elements of sidebar component

--- src/frontend/js/app/sidebar-component.js
+++ src/frontend/js/app/sidebar-component.js
@@ -13,15 +13,18 @@ export class SidebarComponent extends HTMLElement {
     render() {
         this.shadowRoot.innerHTML = `
             <style>
-            /* Styles for the sidebar component */
+                ul {
+                    list-style-type: none;
+                    padding: 0;
+                }
+                li {
+                    padding: 10px;
+                    border-bottom: 1px solid #ccc;
+                }
             </style>
             <div>
                 <h2>Conversations</h2>
-                <ul>
-                    <li>Conversation 1</li>
-                    <li>Conversation 2</li>
-                    <li>Conversation 3</li>
-                </ul>
+                <ul id="conversationList"></ul>
             </div>
         `;
     }
