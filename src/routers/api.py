6f1d1e4b20a48ce7556c84a78ff3fed092b370d4
Tony Rizko <tony@rizkocircle.com>
2024-07-26 20:33:20 -0700

    Add get user route

--- src/routers/api.py
+++ src/routers/api.py
@@ -9,9 +9,15 @@ from models.conversation import create_conversation, get_conversations_by_user_i
 router = APIRouter(prefix="/api")
 
 
-@router.post("/conversation")
-async def conversation():
-    return create_conversation()
+@router.get("/user")
+async def conversation(request: Request, pool = Depends(get_db_pool)):
+    user_session = request.session.get("user")
+    if not user_session:
+        return RedirectResponse(url="/")
+
+    async with pool.acquire() as connection:
+        user = await get_user_by_discord_username(connection, user_session["username"])
+        return user
 
 
 @router.get("/conversations")
