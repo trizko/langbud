71590cc8b7ff2a9681bb73d4bda59ddb6b3019d4
Tony Rizko <tony@rizkocircle.com>
2024-07-27 22:27:24 -0700

    Add constants file with language mappings

---
+++ src/frontend/js/state/constants.js
@@ -0,0 +1,13 @@
+export const LANGUAGE_MAPPING = {
+    "en":  "English",
+    "es":  "Spanish",
+    "es-MX": "Mexican Spanish",
+    "fr":  "French",
+    "de":  "German",
+    "de-ch": "Swiss German",
+    "it":  "Italian",
+    "tr":  "Turkish",
+    "pt-BR": "Brazilian Portuguese",
+    "jp":  "Japanese",
+    "ar":  "Arabic",
+}
