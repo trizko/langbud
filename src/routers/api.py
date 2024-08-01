95c483eb4f21a8ab303a62b70d01794ab860e3b4
Tony Rizko <tony@rizkocircle.com>
2024-07-31 22:29:50 -0700

    Add route for updating active conversation id

--- src/routers/api.py
+++ src/routers/api.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, Request
 from fastapi.responses import RedirectResponse
 
 from dependencies import get_db_pool
-from models.user import get_user_by_discord_username
+from models.user import get_user_by_discord_username, update_user
 from models.conversation import create_conversation, get_conversations_by_user_id
 from models.message import get_messages_by_conversation_id
 
@@ -18,6 +18,19 @@ async def conversation(request: Request, pool = Depends(get_db_pool)):
 
     async with pool.acquire() as connection:
         user = await get_user_by_discord_username(connection, user_session["username"])
+        return user
+
+
+@router.post("/user")
+async def update_active_conversation(request: Request, pool = Depends(get_db_pool)):
+    user_session = request.session.get("user")
+    if not user_session:
+        return RedirectResponse(url="/")
+
+    async with pool.acquire() as connection:
+        user = await get_user_by_discord_username(connection, user_session["username"])
+        data = await request.json()
+        user = await update_user(connection, user.user_id, data["active_conversation_id"])
         return user
 
 
