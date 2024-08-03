a90288c4fe1bb3adc9dedba5293b2543e7ba30be
Tony Rizko <tony@rizkocircle.com>
2024-08-02 17:53:49 -0700

    Refactor api routes to use user session middleware

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,4 +1,12 @@
 from fastapi import Request
+from fastapi.responses import RedirectResponse
 
 async def get_db_pool(request: Request):
     return await request.app.state.db.get_pool()
+
+async def get_user_session(request: Request):
+    user_session = request.session.get("user")
+    if not user_session:
+        return RedirectResponse(url="/")
+
+    return user_session
