a37a73070180ea9c72d03601325ec9affa0b2a98
Tony Rizko <tony@rizkocircle.com>
2024-08-03 22:35:35 -0700

    Add redux like state store

---
+++ src/frontend/js/state/store.js
@@ -0,0 +1,23 @@
+export class Store {
+  constructor(reducer) {
+    this.reducer = reducer;
+    this.state = this.reducer(undefined, {});
+    this.listeners = [];
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  dispatch(action) {
+    this.state = this.reducer(this.state, action);
+    this.listeners.forEach(listener => listener());
+  }
+
+  subscribe(listener) {
+    this.listeners.push(listener);
+    return () => {
+      this.listeners = this.listeners.filter(l => l !== listener);
+    };
+  }
+}
