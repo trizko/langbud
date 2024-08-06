bb132731d2d7b4a1930c48ae9d56fb3b637eff96
Tony Rizko <tony@rizkocircle.com>
2024-08-05 22:03:11 -0700

    Rename components to use standard javascript naming conventions

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,6 +1,6 @@
-import { AppComponent } from './js/app/app-component.js';
-import { SidebarComponent } from './js/app/sidebar-component.js';
-import { ChatComponent } from './js/app/chat-component.js';
+import { AppComponent } from './js/app/appComponent.js';
+import { SidebarComponent } from './js/app/sidebarComponent.js';
+import { ChatComponent } from './js/app/chatComponent.js';
 
 customElements.define('app-component', AppComponent);
 customElements.define('sidebar-component', SidebarComponent);
