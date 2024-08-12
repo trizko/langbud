e73341ea48889fb24242e33295b1fd857e896a6e
Tony Rizko <tony@rizkocircle.com>
2024-08-12 00:32:56 -0700

    Add health check endpoint to server

--- src/routers/static.py
+++ src/routers/static.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, status
 from fastapi.responses import FileResponse, RedirectResponse
 
 from dependencies import get_user_session
@@ -18,3 +18,8 @@ async def index(request: Request):
 @router.get('/chat/')
 async def chat(_ = Depends(get_user_session)):
     return FileResponse("src/frontend/app.html")
+
+
+@router.get('/health', status_code=status.HTTP_200_OK)
+async def healthcheck():
+    return { "status": "OK" }
